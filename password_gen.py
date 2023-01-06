45ae4df7d9a1060246ca98f2a5862d35981d8655
Darrius-W <you@example.com>
2023-01-05 20:11:13 -0600

    Entered schema for password generator

---
+++ password_gen.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+# Event Handlers
+
+# New Window
+# Frame: Base frame to hold all other frames
+
+# Frame: Generate Password
+# Button: Copy Password
+# Label: Display Password
+# Button: Generate New Password
+
+# Frame: Length of Password
+# Label: Widget Header
+# Slider: Adjust length of Generated Password
+# Label: Hold Current Value of the Slider
+
+# Frame: Symbols Included in Password
+# Label: Widget Header
+# Checkboxes: Symbols to be Included in Password
+
+# Execute Mainloop
